334cb0ab05659028aa728d79de9eae7fb1b068dd
Th0masDI0N <thomas.dion2@etu.toulouse-inp.fr>
2025-03-12 09:48:58 +0100

    add model & train

---
+++ api/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+
+class FilmClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super(FilmClassifier, self).__init__()
+
+        # Charger resnet pré-entraîné
+        self.model = models.resnet34(pretrained=True)
+
+        # Adapter la dernière couche au nombre de classes
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, num_classes)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = self.model.to(self.device)
+
+    def forward(self, x):
+        return self.model(x)
