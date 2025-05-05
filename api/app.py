1e17fedbf88725c2faffddfc654306f6e0ea0b89
clgris <gris@insa-toulouse.fr>
2025-05-05 20:28:34 +0200

    ajout de XAI dans app.py et webapp_st.py

--- api/app.py
+++ api/app.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify, request
 from PIL import Image
 import io
 from model import FilmClassifier
+import matplotlib.pyplot as plt
+import numpy as np
+from flask import send_file
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -29,7 +33,7 @@ model.eval()
 transform = transforms.Compose(
         [transforms.Resize((224, 224)),
         transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))])
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -68,6 +72,62 @@ def batch_predict():
         _, predictions = outputs.max(1)
 
     return jsonify({"predictions": predictions.tolist()})
+
+def get_vanilla_grad(img, model):
+    img.retain_grad()
+    output = model(img)
+    output_idx = output.argmax()
+    output_max = output[0, output_idx]
+    output_max.backward()
+    return img.grad
+
+@app.route('/saliency', methods=['POST'])
+def saliency():
+    # Récupérer l'image
+    img_file = request.data #request.files["image"]
+    # img_pil = Image.open(img_file.stream).convert("RGB")
+    # np_img = np.array(img_pil)
+
+    # Transformer en tenseur
+    # img = transform(img_pil).unsqueeze(0).to(device)
+    img_pil = Image.open(io.BytesIO(img_file))
+    np_img = np.array(img_pil)
+
+    # Transform the PIL image
+    tensor = transform(img_pil).to(device)
+    img = tensor.unsqueeze(0)
+    img.requires_grad_()
+
+    # Calcul des gradients avec bruit
+    stdev_spread = 0.15
+    n_samples = 100
+    stdev = stdev_spread * (img.max() - img.min())
+    total_gradients = torch.zeros_like(img, device='cuda')
+
+    for _ in range(n_samples):
+        noise = np.random.normal(0, stdev.item(), img.shape).astype(np.float32)
+        noisy_img = img + torch.tensor(noise, device='cuda', requires_grad=True)
+        grad = get_vanilla_grad(noisy_img, model)
+        total_gradients += grad * grad
+
+    total_gradients /= n_samples
+    saliency, _ = torch.max(total_gradients.abs(), dim=1)
+    saliency = saliency.squeeze(0).cpu().numpy()
+
+    # Créer l’image de la saliency map
+    fig, ax = plt.subplots(1, 2, figsize=(15, 10))
+    ax[0].imshow(np_img)
+    ax[0].axis('off')
+    ax[1].imshow(saliency, cmap='hot')
+    ax[1].axis('off')
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    plt.close(fig)
+
+    return send_file(buf, mimetype='image/png')
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host="0.0.0.0")
--- ihm/webapp_st.py
+++ ihm/webapp_st.py
@@ -6,6 +6,7 @@ from PIL import Image
 # Définir les URLs des APIs
 GENRE_API_URL = "http://api:5000/predict"
 SIMILAR_API_URL = "http://api:5001/predict"
+SALIENCY_API_URL = "http://api:5000/saliency"
 
 # Mapping des indices vers les noms des genres
 GENRE_LABELS = {
@@ -49,6 +50,22 @@ def find_similar(image):
     except Exception as e:
         return f"Erreur: {str(e)}"
 
+def get_saliency_map(image):
+    try:
+        img_binary = io.BytesIO()
+        image.save(img_binary, format="PNG")
+        # files = {'image': img_binary}
+        response = requests.post(SALIENCY_API_URL, data=img_binary.getvalue())
+        
+        if response.status_code == 200:
+            return Image.open(io.BytesIO(response.content))
+        else:
+            return None
+    except Exception as e:
+       	print(e)
+       	return None
+
+#Interface
 st.title("Prédiction de Genre et Recommandations de Films")
 uploaded_image = st.file_uploader("Uploader une image de poster de film", type=["png", "jpg", "jpeg"])
 
@@ -68,3 +85,10 @@ if uploaded_image:
                 st.image(img, use_column_width=True)
         else:
             st.write("Aucune recommandation trouvée.")
+    if st.button("Afficher la saliency map"):
+        saliency_map = get_saliency_map(image)
+        if saliency_map:
+            st.image(saliency_map, caption="Carte de saillance (saliency map)", use_column_width=True)
+        else:
+            st.write("Impossible de générer la saliency map.")
+    
