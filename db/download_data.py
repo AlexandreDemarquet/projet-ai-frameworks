486389f6acdf4df4d51efc699fd55f1dcf83cf9b
AlexandreDemarquet <alex.demarquet@gmail.com>
2025-05-31 16:03:57 +0200

    propre dans le code

--- db/download_data.py
+++ db/download_data.py
@@ -2,10 +2,8 @@ import os
 import zipfile
 import gdown
 
-# URL du fichier Google Drive
 DRIVE_URL = "https://drive.google.com/uc?export=download&id=1-1OSGlN2EOqyZuehBgpgI8FNOtK-caYf"  # ID extrait de l'URL
 
-# Chemin du dossier où les images seront stockées
 DATA_DIR = "data"
 ZIP_PATH = "data/posters.zip"
 
@@ -16,10 +14,8 @@ os.makedirs("data", exist_ok=True)
 # if not os.path.exists(DATA_DIR):
 #     print("📦 Téléchargement des images depuis Google Drive...")
     
-# Télécharger le fichier depuis Google Drive
 gdown.download(DRIVE_URL, ZIP_PATH, quiet=False)
 
-# Extraction du fichier ZIP
 with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
     zip_ref.extractall(DATA_DIR)
 print("✅ Données téléchargées et extraites.")
